2d2c3aa057f805474a89bc005c9a9992de680ec9
DistritoCapital2 <distritocapitalmovil2018@gmail.com>
2018-08-31 09:57:40 -0500

    chore: Se suben los fragments y se dejan listos para trabajar.

---
+++ app/src/main/java/com/example/worldskills/turisapp/fragments/HotelFragment.java
@@ -0,0 +1,74 @@
+package com.example.worldskills.turisapp.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.worldskills.turisapp.R;
+
+
+public class HotelFragment extends Fragment {
+
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public HotelFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static HotelFragment newInstance(String param1, String param2) {
+        HotelFragment fragment = new HotelFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hotel, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/com/example/worldskills/turisapp/fragments/MainActivity.java
+++ app/src/main/java/com/example/worldskills/turisapp/fragments/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Agregar titulo fragment Inicio
+        toolbar.setTitle("Inicio");
         setSupportActionBar(toolbar);
 
 
@@ -58,12 +60,9 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -74,7 +73,7 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        //Menus nav del navigation drawer.
         int id = item.getItemId();
 
         if (id == R.id.nav_inicio) {
