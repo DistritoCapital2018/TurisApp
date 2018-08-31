7b03794fc12a58b81220dfb226144c05d6106698
DistritoCapital2 <distritocapitalmovil2018@gmail.com>
2018-08-31 08:36:57 -0500

    chore: Se alista el splash, aun no terminado.

--- app/src/main/java/com/example/worldskills/turisapp/SplashActivity.java
+++ app/src/main/java/com/example/worldskills/turisapp/SplashActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    //Se instancian los componentes del layout.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
