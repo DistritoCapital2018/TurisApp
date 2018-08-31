ab5f09ffc92474a519ae0c10a943a2d8340d3380
Distrito Capital <wskdistritocapital2018@gmail.com>
2018-08-31 13:20:45 -0500

    chore: Se sube typografia

--- app/src/main/java/com/example/worldskills/turisapp/SplashActivity.java
+++ app/src/main/java/com/example/worldskills/turisapp/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.example.worldskills.turisapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -15,6 +16,9 @@ public class SplashActivity extends AppCompatActivity {
     ImageView imgLogo;
     TextView tvWSS;
 
+    Typeface typeface;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,10 @@ public class SplashActivity extends AppCompatActivity {
         //Instancio el componente junto con el xml.
         imgLogo = findViewById(R.id.imgLogoT);
         tvWSS = findViewById(R.id.tvWSS);
+
+        typeface = Typeface.createFromAsset(getAssets(),"fonts/poorich.ttf");
+
+        tvWSS.setTypeface(typeface);
 
         //Creo un nuevo hilo
         Thread hilo = new Thread(){
--- app/src/main/java/com/example/worldskills/turisapp/fragments/InicioFragment.java
+++ app/src/main/java/com/example/worldskills/turisapp/fragments/InicioFragment.java
@@ -1,12 +1,14 @@
 package com.example.worldskills.turisapp.fragments;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.worldskills.turisapp.R;
 
@@ -15,7 +17,7 @@ public class InicioFragment extends Fragment {
 
     //Instancio los componentes que nesecito
     View view;
-
+    Typeface typeface;
 
     private OnFragmentInteractionListener mListener;
 
@@ -42,6 +44,15 @@ public class InicioFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_inicio, container, false);
+        try {
+
+            typeface = Typeface.createFromAsset(getContext().getAssets(),"fonts/poorich.ttf");
+        }catch (Exception e)
+        {
+            Toast.makeText(getContext(),"Error en la Tipografia",Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
 
         return view;
     }
